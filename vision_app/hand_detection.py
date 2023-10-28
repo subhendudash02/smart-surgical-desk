0c3a3f2113a35dae27a8ae7207c499846318dbcb
Subhendu Dash <sdash29102@gmail.com>
2023-10-28 16:36:36 +0530

    support multithreading

--- vision_app/hand_detection.py
+++ vision_app/hand_detection.py
@@ -2,96 +2,119 @@ import cv2
 import mediapipe as mp
 import components
 import requests
+import threading
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-api_url = "http://192.168.117.66:5000/api/hand_gesture/"
+api_url = "https://smart-surgical-desk.vercel.app/api/hand_gesture"
 
 # For webcam input:
 cap = cv2.VideoCapture(0)
-with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
+fingerCount = 0
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
+def count_fingers():
 
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+  with mp_hands.Hands(
+      model_complexity=0,
+      min_detection_confidence=0.5,
+      min_tracking_confidence=0.5) as hands:
+    while cap.isOpened():
+      success, image = cap.read()
+      if not success:
+        print("Ignoring empty camera frame.")
+        # If loading a video, use 'break' instead of 'continue'.
+        continue
 
-    # Initially set finger count to 0 for each cap
-    fingerCount = 0
+      # To improve performance, optionally mark the image as not writeable to
+      # pass by reference.
+      image.flags.writeable = False
+      image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+      results = hands.process(image)
 
-    if results.multi_hand_landmarks:
+      # Draw the hand annotations on the image.
+      image.flags.writeable = True
+      image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-      for hand_landmarks in results.multi_hand_landmarks:
-        # Get hand index to check label (left or right)
-        handIndex = results.multi_hand_landmarks.index(hand_landmarks)
-        handLabel = results.multi_handedness[handIndex].classification[0].label
+      # Initially set finger count to 0 for each cap
+      global fingerCount
+      fingerCount = 0
 
-        # Set variable to keep landmarks positions (x and y)
-        handLandmarks = []
+      if results.multi_hand_landmarks:
 
-        # Fill list with x and y positions of each landmark
-        for landmarks in hand_landmarks.landmark:
-          handLandmarks.append([landmarks.x, landmarks.y])
+        for hand_landmarks in results.multi_hand_landmarks:
+          # Get hand index to check label (left or right)
+          handIndex = results.multi_hand_landmarks.index(hand_landmarks)
+          handLabel = results.multi_handedness[handIndex].classification[0].label
 
-        # Test conditions for each finger: Count is increased if finger is 
-        #   considered raised.
-        # Thumb: TIP x position must be greater or lower than IP x position, 
-        #   deppeding on hand label.
-        if handLabel == "Left" and handLandmarks[4][0] > handLandmarks[3][0]:
-          fingerCount = fingerCount+1
-        elif handLabel == "Right" and handLandmarks[4][0] < handLandmarks[3][0]:
-          fingerCount = fingerCount+1
+          # Set variable to keep landmarks positions (x and y)
+          handLandmarks = []
 
-        # Other fingers: TIP y position must be lower than PIP y position, 
-        #   as image origin is in the upper left corner.
-        if handLandmarks[8][1] < handLandmarks[6][1]:       #Index finger
-          fingerCount = fingerCount+1
-        if handLandmarks[12][1] < handLandmarks[10][1]:     #Middle finger
-          fingerCount = fingerCount+1
-        if handLandmarks[16][1] < handLandmarks[14][1]:     #Ring finger
-          fingerCount = fingerCount+1
-        if handLandmarks[20][1] < handLandmarks[18][1]:     #Pinky
-          fingerCount = fingerCount+1
+          # Fill list with x and y positions of each landmark
+          for landmarks in hand_landmarks.landmark:
+            handLandmarks.append([landmarks.x, landmarks.y])
 
-        # Draw hand landmarks 
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
+          # Test conditions for each finger: Count is increased if finger is 
+          #   considered raised.
+          # Thumb: TIP x position must be greater or lower than IP x position, 
+          #   deppeding on hand label.
+          if handLabel == "Left" and handLandmarks[4][0] > handLandmarks[3][0]:
+            fingerCount = fingerCount+1
+          elif handLabel == "Right" and handLandmarks[4][0] < handLandmarks[3][0]:
+            fingerCount = fingerCount+1
 
-    # Display finger count
-    cv2.putText(image, str(components.desk_components(fingerCount)), (50, 450), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 10)
+          # Other fingers: TIP y position must be lower than PIP y position, 
+          #   as image origin is in the upper left corner.
+          if handLandmarks[8][1] < handLandmarks[6][1]:       #Index finger
+            fingerCount = fingerCount+1
+          if handLandmarks[12][1] < handLandmarks[10][1]:     #Middle finger
+            fingerCount = fingerCount+1
+          if handLandmarks[16][1] < handLandmarks[14][1]:     #Ring finger
+            fingerCount = fingerCount+1
+          if handLandmarks[20][1] < handLandmarks[18][1]:     #Pinky
+            fingerCount = fingerCount+1
 
-    if fingerCount > 0 and fingerCount < 4:
-      requests.post(api_url, json={
-        "finger": fingerCount, 
-        "component": components.desk_components(fingerCount)})
-    else:
-      requests.post(api_url, json={
-        "finger": 0,
-        "component": "None"
-      })
+          # Draw hand landmarks 
+          mp_drawing.draw_landmarks(
+              image,
+              hand_landmarks,
+              mp_hands.HAND_CONNECTIONS,
+              mp_drawing_styles.get_default_hand_landmarks_style(),
+              mp_drawing_styles.get_default_hand_connections_style())
 
-    # Display image
-    cv2.imshow('Smart Surgical Desk', image)
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
-cap.release()
+      # Display finger count
+      cv2.putText(image, str(components.desk_components(fingerCount)), (50, 450), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 10)
+
+      # Display image
+      cv2.imshow('Smart Surgical Desk', image)
+      if cv2.waitKey(5) & 0xFF == 27:
+        break
+  cap.release()
+
+
+def send_api_requests():
+    while True:
+        if fingerCount > 0 and fingerCount < 4:
+            requests.post(api_url, json={
+                "finger": fingerCount, 
+                "component": components.desk_components(fingerCount)})
+        else:
+            requests.post(api_url, json={
+                "finger": 0,
+                "component": "None"
+            })
+
+frame_thread = threading.Thread(target=count_fingers)
+api_thread = threading.Thread(target=send_api_requests)
+
+# Start the threads
+frame_thread.start()
+api_thread.start()
+
+# Wait for the threads to finish
+frame_thread.join()
+api_thread.join()
+
+# Clean up
+cv2.destroyAllWindows()
